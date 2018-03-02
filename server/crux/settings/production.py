3d73f55193da74d6843bac85a2b5b3f16901f430
Sabareesh Natarajan <sab.natarajan@gmail.com>
2018-03-01 19:57:43 -0500

    Test CSRF

--- server/crux/settings/production.py
+++ server/crux/settings/production.py
@@ -4,7 +4,8 @@ DEBUG = False
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 ALLOWED_HOSTS = [
-    'crux-nebigdatahub.herokuapp.com'
+    'crux-nebigdatahub.herokuapp.com',
+    'crux-staging-nebigdatahub.herokuapp.com',
 ]
 
 CSRF_COOKIE_SECURE = True
