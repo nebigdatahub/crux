ae58cf5795fe6f2f66d198b4b6dbcf9d6dc238bd
Sabareesh Natarajan <sab.natarajan@gmail.com>
2018-05-24 00:55:33 -0500

    -

--- server/scripts/release.sh
+++ server/scripts/release.sh
@@ -5,10 +5,10 @@ echo "|    Running migrations    |"
 echo "+--------------------------+"
 python server/manage.py migrate
 
-echo "+--------------------------+"
-echo "|       Loading data       |"
-echo "+--------------------------+"
-python server/manage.py loaddata users
-python server/manage.py loaddata datasets
-python server/manage.py loaddata analyses
-python server/manage.py loaddata files
+# echo "+--------------------------+"
+# echo "|       Loading data       |"
+# echo "+--------------------------+"
+# python server/manage.py loaddata users
+# python server/manage.py loaddata datasets
+# python server/manage.py loaddata analyses
+# python server/manage.py loaddata files
