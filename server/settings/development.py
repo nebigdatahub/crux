b1065f3acae8feb861c51024292ff301f367e4b1
raphael junior <ro2295@columbia.edu>
2018-11-01 21:02:48 -0400

    Travis fix

--- server/settings/development.py
+++ server/settings/development.py
@@ -4,7 +4,7 @@ DEBUG = True
 
 with open(os.path.join(BASE_DIR, 'settings', 'secret_key.txt')) as f:
     SECRET_KEY = f.read().strip()
-
+SECRET_KEY = 'secretkeyfortravis'
 CORS_ORIGIN_ALLOW_ALL = True
 
 ALLOWED_HOSTS = [
