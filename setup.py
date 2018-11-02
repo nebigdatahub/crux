ecc4196bdc9532776ea22a1dd5a35666facafc33
raphael junior <ro2295@columbia.edu>
2018-11-01 20:02:20 -0400

    Travis fix

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='Crux',
+      version='0.0.1',
+      description='CRUX',
+      author='',
+      author_email='',
+      url='',
+      packages=find_packages(),
+     )
