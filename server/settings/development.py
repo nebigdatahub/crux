6fdeb176d41c3cf8b06e2a5ca7f334b78389cb62
raphael junior <ro2295@columbia.edu>
2018-11-06 15:30:33 -0500

    travis

--- server/settings/development.py
+++ server/settings/development.py
@@ -4,7 +4,6 @@ DEBUG = True
 
 with open(os.path.join(BASE_DIR, 'settings', 'secret_key.txt')) as f:
     SECRET_KEY = f.read().strip()
-SECRET_KEY = "secretkeyfortravis"
 CORS_ORIGIN_ALLOW_ALL = True
 
 if os.getenv('BUILD_ON_TRAVIS', None):
--- server/settings/staging.py
+++ server/settings/staging.py
@@ -1,20 +1,6 @@
 from .common import *
 import dj_database_url
 
-if os.getenv('BUILD_ON_TRAVIS', None):
-    SECRET_KEY = "SecretKeyForUseOnTravis"
-    DEBUG = False
-    TEMPLATE_DEBUG = True
-
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'travis_ci_db',
-            'USER': 'travis',
-            'PASSWORD': '',
-            'HOST': '127.0.0.1',
-        }
-    }
 DEBUG = True
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
