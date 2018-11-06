3c77385a7b5c1d355dc706d33d2a12ad2714c631
raphael junior <ro2295@columbia.edu>
2018-11-06 15:16:07 -0500

    Travis fix

--- server/settings/development.py
+++ server/settings/development.py
@@ -7,6 +7,11 @@ with open(os.path.join(BASE_DIR, 'settings', 'secret_key.txt')) as f:
 SECRET_KEY = "secretkeyfortravis"
 CORS_ORIGIN_ALLOW_ALL = True
 
+if os.getenv('BUILD_ON_TRAVIS', None):
+    SECRET_KEY = "SecretKeyForUseOnTravis"
+    DEBUG = False
+    TEMPLATE_DEBUG = True
+
 ALLOWED_HOSTS = [
     'crux',
     'localhost'
