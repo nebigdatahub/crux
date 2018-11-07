44ab6de3662e795a1d0b8b64153e4d9d1f13e6d2
raphael junior <ro2295@columbia.edu>
2018-11-06 19:47:11 -0500

    travis

--- server/app/admin/user.py
+++ server/app/admin/user.py
@@ -8,11 +8,11 @@ from ..models import User
 class UserAdmin(ModelAdmin):
     fieldsets = (
         (None, {'fields': ('email', 'password',)}),
-        (_('Personal info'), {
+        ('Personal info', {
             'fields': ('first_name', 'last_name', 'email',)}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions',)}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined',)})
+        ('Important dates', {'fields': ('last_login', 'date_joined',)})
     )
     add_fieldsets = (
         (None, {
