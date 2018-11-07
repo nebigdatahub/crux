934fe33022b3335cc3cfdbc80e2e9355cdde1606
raphael junior <ro2295@columbia.edu>
2018-11-07 15:05:51 -0500

    travis

--- server/app/admin/user.py
+++ server/app/admin/user.py
@@ -6,21 +6,4 @@ from ..models import User
 
 @register(User)
 class UserAdmin(ModelAdmin):
-    fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-    )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'password1', 'password2'),
-        }),
-    )
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
-    search_fields = ('username', 'first_name', 'last_name', 'email')
-    ordering = ('username',)
-    filter_horizontal = ('groups', 'user_permissions',)
+   pass
